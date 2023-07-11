f1736659a55b9058dc0fbbe0acee3001c4381212
Samuel-Boehm <samuel-boehm@web.de>
2023-07-11 14:15:37 +0200

    create module for better imports

---
+++ setup.py
@@ -0,0 +1,7 @@
+# Project: EEG-GAN
+# Author: Samuel Boehm
+# E-Mail: <samuel-boehm@web.de>
+
+from setuptools import setup, find_packages
+
+setup(name='eeggan', version='1.0', packages=find_packages())
